adab029cc11476dba9da39c0331855d9880d665f
Moazam05 <salmanmoazam08@gmail.com>
2024-10-07 15:27:14 +0500

    inline style removed

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -6,17 +6,10 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Home = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: themeColors.WHITE,
-      }}>
-      <View
-        style={{
-          flex: 0.7,
-        }}>
+    <View style={styles.container}>
+      <View style={styles.mapContainer}>
         <MapView
-          style={{flex: 1}}
+          style={styles.map}
           provider={PROVIDER_GOOGLE}
           initialRegion={{
             latitude: 37.78825,
@@ -26,53 +19,67 @@ const Home = () => {
           }}
         />
 
-        <View
-          style={{
-            position: 'absolute',
-            top: 30,
-            right: 20,
-            left: 20,
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: 15,
-          }}>
-          <View
-            style={{
-              backgroundColor: themeColors.WHITE,
-              width: 40,
-              height: 40,
-              borderRadius: 50,
-              justifyContent: 'center',
-              alignItems: 'center',
-              shadowColor: themeColors.BLACK,
-              shadowOffset: {
-                width: 0,
-                height: 2,
-              },
-              shadowOpacity: 0.25,
-              shadowRadius: 3.84,
-              elevation: 5,
-            }}>
+        <View style={styles.searchBarContainer}>
+          <View style={styles.menuButton}>
             <Ionicons name="menu" size={25} color={themeColors.GRAY} />
           </View>
 
-          <View
-            style={{
-              backgroundColor: themeColors.WHITE,
-              flex: 1,
-              borderRadius: 30,
-              paddingHorizontal: 15,
-            }}>
+          <View style={styles.searchBar}>
             <TextInput value="" placeholder="Current Location" />
           </View>
         </View>
       </View>
 
-      <View style={{flex: 0.3}}></View>
+      <View style={styles.bottomContainer} />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: themeColors.WHITE,
+  },
+  mapContainer: {
+    flex: 0.7,
+  },
+  map: {
+    flex: 1,
+  },
+  searchBarContainer: {
+    position: 'absolute',
+    top: 30,
+    right: 20,
+    left: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+  },
+  menuButton: {
+    backgroundColor: themeColors.WHITE,
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: themeColors.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  searchBar: {
+    backgroundColor: themeColors.WHITE,
+    flex: 1,
+    borderRadius: 30,
+    paddingHorizontal: 15,
+  },
+  bottomContainer: {
+    flex: 0.3,
+  },
+});
