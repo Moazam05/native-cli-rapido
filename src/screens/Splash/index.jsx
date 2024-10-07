35f87392ade8b1eea753adad4d1ad591fa864a12
Moazam05 <salmanmoazam08@gmail.com>
2024-10-07 14:18:46 +0500

    vector icon installed installed

--- src/screens/Splash/index.jsx
+++ src/screens/Splash/index.jsx
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Splash = () => {
   return (
     <View>
       <Text>Splash</Text>
+      <Icon name="facebook" size={30} color="#4F8EF7" />
     </View>
   );
 };
