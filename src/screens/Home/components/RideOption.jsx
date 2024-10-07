a337b1248f5b0cd5067381cdff129056916d4e65
Moazam05 <salmanmoazam08@gmail.com>
2024-10-08 01:01:23 +0500

    inline style removed

--- src/screens/Home/components/RideOption.jsx
+++ src/screens/Home/components/RideOption.jsx
@@ -1,4 +1,4 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import {themeColors} from '../../../constants/colors';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -16,53 +16,57 @@ const RideOption = ({iconName, label, distance}) => {
     const fare = dis * fareRates[vehicle];
     return fare.toFixed(0);
   }
+
   return (
-    <TouchableOpacity
-      style={{
-        flex: 1,
-        marginHorizontal: 20,
-      }}>
-      <View
-        style={{
-          backgroundColor: themeColors.WHITE,
-          borderRadius: 35,
-          paddingVertical: 5,
-          paddingHorizontal: 10,
-        }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 10,
-            }}>
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.card}>
+        <View style={styles.row}>
+          <View style={styles.iconTextContainer}>
             <Ionicons name={iconName} size={23} color={themeColors.BLACK} />
-            <Text
-              style={{
-                fontSize: 14,
-                fontFamily: Fonts.REGULAR,
-                color: themeColors.BLACK,
-              }}>
-              {label}
-            </Text>
+            <Text style={styles.label}>{label}</Text>
           </View>
-          <Text
-            style={{
-              fontSize: 16,
-              fontFamily: Fonts.MEDIUM,
-              color: themeColors.BLACK,
-            }}>
-            Rs. {thousandSeparator(calculateFare(distance, label))}
-          </Text>
+          {distance > 0 && (
+            <Text style={styles.fare}>
+              Rs. {thousandSeparator(calculateFare(distance, label))}
+            </Text>
+          )}
         </View>
       </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+  card: {
+    backgroundColor: themeColors.WHITE,
+    borderRadius: 35,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  iconTextContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  label: {
+    fontSize: 14,
+    fontFamily: Fonts.REGULAR,
+    color: themeColors.BLACK,
+  },
+  fare: {
+    fontSize: 15,
+    fontFamily: Fonts.MEDIUM,
+    color: themeColors.BLACK,
+  },
+});
 
 export default RideOption;
