bd884a472448e301587ecafcd4ce23f42c8a8747
Moazam05 <salmanmoazam08@gmail.com>
2024-10-07 12:43:03 +0500

    routes created

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -1,11 +1,20 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import Splash from '../screens/Splash';
+
+const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <View>
-      <Text>AppNavigator</Text>
-    </View>
+    <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Splash" component={Splash} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
   );
 };
 
