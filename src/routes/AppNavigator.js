4897dfa163a808500032501c1c401f6703fe98a2
Moazam05 <salmanmoazam08@gmail.com>
2024-10-09 01:38:35 +0500

    refactor

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -3,8 +3,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Splash from '../screens/Splash';
 import Destination from '../screens/Home/components/Destination';
-import HomeTwo from '../screens/Home/HomeTwo';
-import HomeThree from '../screens/Home/HomeThree';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,8 +15,6 @@ const AppNavigator = () => {
       }}>
       <Stack.Screen name="Splash" component={Splash} />
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="HomeTwo" component={HomeTwo} />
-      <Stack.Screen name="HomeThree" component={HomeThree} />
       <Stack.Screen name="Destination" component={Destination} />
     </Stack.Navigator>
   );
