b01b0bb673576e81ba4be1518bf7f9458517314b
Moazam05 <salmanmoazam08@gmail.com>
2024-10-10 21:30:40 +0500

    provider added

--- App.js
+++ App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/routes/AppNavigator';
 import ToastComponent from './src/components/ToastComponent';
-import {persistor} from './src/redux/store';
+import {persistor, store} from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <PersistGate loading={null} persistor={persistor}>
-        <AppNavigator />
-      </PersistGate>
-      <ToastComponent />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppNavigator />
+        </PersistGate>
+        <ToastComponent />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
