8527851f905b8b1926ba9753994a05ef7fe23bc1
Moazam05 <salmanmoazam08@gmail.com>
2024-10-10 21:12:19 +0500

    pre home set

--- src/screens/Home/PreHome.jsx
+++ src/screens/Home/PreHome.jsx
@@ -57,10 +57,13 @@ const PreHome = () => {
       }
     };
 
-    AppState.addEventListener('change', handleAppStateChange);
+    const subscription = AppState.addEventListener(
+      'change',
+      handleAppStateChange,
+    );
 
     return () => {
-      AppState.removeEventListener('change', handleAppStateChange);
+      subscription.remove(); // Use the remove method on the subscription
     };
   }, []);
 
